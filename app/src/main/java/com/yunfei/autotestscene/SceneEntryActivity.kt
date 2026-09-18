47de9412edde212809e1e3773beb0f09c6df3815
yunfei00 <goodman_yunfei@163.com>
2026-09-18 19:35:50 +0800

    feat: arm scene watchdog from adb entry

--- app/src/main/java/com/yunfei/autotestscene/SceneEntryActivity.kt
+++ app/src/main/java/com/yunfei/autotestscene/SceneEntryActivity.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.yunfei.autotestscene.scene.SceneIds
 import com.yunfei.autotestscene.scene.SceneRouter
+import com.yunfei.autotestscene.scene.SceneWatchdog
 
 class SceneEntryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val scene = intent.getStringExtra(SceneIds.EXTRA_SCENE)
-        val duration = intent.getIntExtra(SceneIds.EXTRA_DURATION, 30)
+        val duration = intent.getIntExtra(SceneIds.EXTRA_DURATION, 0)
         val style = intent.getStringExtra(SceneIds.EXTRA_STYLE)
         val particles = intent.getIntExtra(SceneIds.EXTRA_PARTICLES, 250)
         val cpuThreads = intent.getIntExtra(SceneIds.EXTRA_CPU_THREADS, 2)
         val audio = intent.getBooleanExtra(SceneIds.EXTRA_AUDIO, false)
         val vibration = intent.getBooleanExtra(SceneIds.EXTRA_VIBRATION, false)
-        SceneRouter.openScene(
+        val started = SceneRouter.openScene(
             context = this,
             sceneId = scene,
             duration = duration,
@@ -26,6 +27,9 @@ class SceneEntryActivity : ComponentActivity() {
             audio = audio,
             vibration = vibration
         )
+        if (started) {
+            SceneWatchdog.arm(applicationContext, duration)
+        }
         finish()
     }
 }
